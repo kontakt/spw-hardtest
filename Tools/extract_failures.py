c427c3cedbd614d05922dca7bcfa2de866259b7c
Connor Huffine <chuffine@gmail.com>
2026-05-30 16:54:44 -0400

    move to python

---
+++ Tools/extract_failures.py
@@ -0,0 +1,35 @@
+import json
+import xmltodict
+
+def item_generator(json_input):
+    if isinstance(json_input, dict):
+        for key, value in json_input.items():
+            if key == 'test-case':
+                for key2, value2 in json_input.items():
+                    if key2 == '@result' and value2 == 'Failed':
+                        yield {key: value}
+            else:
+                yield from item_generator(value)
+    elif isinstance(json_input, list):
+        for item in json_input:
+            yield from item_generator(item)
+
+def extract(filename):
+    with open(filename, 'r') as xml_file:
+        xml_data = xmltodict.parse(xml_file.read())
+
+    failures = []
+    for item in item_generator(xml_data['test-run']):
+        failures.append(item)
+
+    return failures
+
+all_fails = []
+all_fails.append(extract('./test_results/logs/Content.Tests.xml'))
+all_fails.append(extract('./test_results/logs/Content.IntegrationTests.xml'))
+
+json_data = json.dumps(all_fails, indent=4)
+print(json_data)
+
+with open('outfile.txt', 'w') as f:
+    f.write(json_data)
