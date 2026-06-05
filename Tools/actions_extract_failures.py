ba5d33cd4e82f2ed380683256247748cb696a61b
Connor Huffine <chuffine@gmail.com>
2026-06-04 20:44:16 -0400

    improve python

--- Tools/actions_extract_failures.py
+++ Tools/actions_extract_failures.py
@@ -2,13 +2,26 @@ import os
 import json
 import xmltodict
 
+def testcase_proc(json_input):
+    """Recursively process a test-case to extract all failures.
+
+    This function is a helper that expects to be fed with extracted test-case elements.
+    """
+    if isinstance(json_input, dict):
+        for key, value in json_input.items():
+            if key == '@result' and value == 'Failed':
+                print(json_input)
+                yield json_input
+    elif isinstance(json_input, list):
+        for item in json_input:
+            yield from testcase_proc(item)
+
 def item_generator(json_input):
+    """Recursively process the test report extract all failures."""
     if isinstance(json_input, dict):
         for key, value in json_input.items():
             if key == 'test-case':
-                for key2, value2 in json_input.items():
-                    if key2 == '@result' and value2 == 'Failed':
-                        yield {key: value}
+                yield from testcase_proc(value)
             else:
                 yield from item_generator(value)
     elif isinstance(json_input, list):
@@ -16,7 +29,8 @@ def item_generator(json_input):
             yield from item_generator(item)
 
 def extract(filename):
-    with open(filename, 'r') as xml_file:
+    """Extract all failures from an XML file."""
+    with open(filename, 'r', encoding='utf-8') as xml_file:
         xml_data = xmltodict.parse(xml_file.read())
 
     failures = []
@@ -29,20 +43,21 @@ all_fails = []
 all_fails.extend(extract('./test_results/logs/Content.Tests.xml'))
 all_fails.extend(extract('./test_results/logs/Content.IntegrationTests.xml'))
 
+# Create the list of processed failures to create a matrix for later jobs.
 matrix = []
 for fail in all_fails:
     matrix.append(
         {
-            'name': fail['test-case']['@name'],
-            'fullname': fail['test-case']['@fullname'],
-            'failure': fail['test-case']['failure']['message'],
-            'output': fail['test-case']['output']
+            'name': fail['@name'],
+            'fullname': fail['@fullname'],
+            'failure': fail['failure']['message'],
+            'output': fail['output']
         }
     )
 
+# Clean up the output to ensure that no '@' symbols escape.
 json_data = json.dumps(matrix).replace('@', '')
 
-print(json_data)
-
+# Write to the action step output
 with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
     f.write(f'matrix={json_data}\n')
